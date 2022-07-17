f225a6aefd34d23ad54333843d9f580e14c44574
Kostiantyn Rymar <subradiance@gmail.com>
2022-07-17 17:09:47 +0300

    Создали model Post

--- App/Controllers/BlogController.php
+++ App/Controllers/BlogController.php
@@ -1,8 +1,12 @@
 <?php
 namespace App\Controllers;
 
+use App\Models\Post;
+
 class BlogController extends Controller {
     public function index() {
+        $post = new Post;
+
         $this->render('Blog');
     }
 }
--- App/Models/Model.php
+++ App/Models/Model.php
@@ -3,5 +3,20 @@
 namespace App\Models;
 
 class Model {
-    
+    protected $name;
+    protected $data;
+
+    private function loadJson() {
+        $file_name = ROOTPATH.'/Database/'.$this->name.'.json';
+
+        $file = fopen($file_name, 'r');
+        $this->data = json_decode(fread($file, filesize($file_name)));
+        fclose($file);
+    }
+
+    public function __construct() {
+        $this->loadJson();
+
+        print_r($this->data);
+    }
 }
